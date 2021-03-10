a0332b74bf41fe036b4765cd90b000f234aaa38c
AbhitanSaha <72934030+AbhitanSaha@users.noreply.github.com>
2021-03-10 21:12:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function preload(){
-
+     moustache=loadImage("https://postimg.cc/bsV5tR30")
 }
 function setup(){
     canvas = createCanvas(300,300);
@@ -11,7 +11,8 @@ function setup(){
     poseNet.on('pose',getPoses);
 }
 function draw(){
-image(video,0,0,300,300);
+    image(video,0,0,300,300);
+    image(clown_nose,noseX-15,noseY,30,30);
 }
 function take_snapshot(){
     save('my_filter_is_UNSTOPABLE.png');
@@ -22,7 +23,9 @@ function modelLoaded(){
 function getPoses(results){
     if (results.length>0) {
         console.log(results);
-        console.log("nose x ="+results[0].pose.nose.x);
-        console.log("nose y ="+results[0].pose.nose.y);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("nose x ="+noseX);
+        console.log("nose y ="+noseY);
     } 
 }
